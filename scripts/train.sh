46f34a0844da10fe4cf1ce95a504f66ac17cb171
JC Recio <juan.carlos.recio.abad@gmail.com>
2025-06-08 14:26:30 +0200

    Fix path

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
-python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_env run_train.py \
+python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_env scripts/run_train.py \
 --diff_steps 2000 \
 --lr 0.0001 \
 --learning_steps 50000 \
