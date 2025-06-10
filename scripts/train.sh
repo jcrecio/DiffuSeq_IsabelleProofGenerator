0c91cfcb5d49b0f61716e00dd816e2e6d78c6651
JC Recio <juan.carlos.recio.abad@gmail.com>
2025-06-10 21:12:08 +0200

    Some fixes

--- scripts/train.sh
+++ scripts/train.sh
@@ -8,7 +8,7 @@ python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_
 --hidden_dim 128 \
 --bsz 2048 \
 --dataset qqp \
---data_dir {path-to-datasets} \
+--data_dir /home/jcrecio/repos/DiffuSeq_IsabelleProofGenerator/datasets/proofs \
 --vocab bert \
 --seq_len 128 \
 --schedule_sampler lossaware \
