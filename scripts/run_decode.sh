52b73a1f44724bd56fb507099695da0a055b8953
JC Recio <juan.carlos.recio.abad@gmail.com>
2025-06-11 21:11:53 +0200

    Refactor

--- scripts/run_decode.sh
+++ scripts/run_decode.sh
@@ -1,4 +1,4 @@
 python -u run_decode.py \
---model_dir diffusion_models/{name-of-model-folder} \
+--model_dir diffusion_models/$1 \
 --seed 123 \
 --split test
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,5 @@
+# $1 = /home/jcrecio/repos/DiffuSeq_IsabelleProofGenerator/datasets/proofs
+
 python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_env scripts/run_train.py \
 --diff_steps 2000 \
 --lr 0.0001 \
@@ -8,7 +10,7 @@ python -m torch.distributed.launch --nproc_per_node=4 --master_port=12233 --use_
 --hidden_dim 128 \
 --bsz 2048 \
 --dataset qqp \
---data_dir /home/jcrecio/repos/DiffuSeq_IsabelleProofGenerator/datasets/proofs \
+--data_dir $1 \
 --vocab bert \
 --seq_len 128 \
 --schedule_sampler lossaware \
